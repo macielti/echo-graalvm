1492e8f72940c358cd32a85e08af976b7d7a6cea
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2025-12-25 08:06:27 -0300

    add help command

--- src/echo_graalvm/diplomat/telegram/consumer.clj
+++ src/echo_graalvm/diplomat/telegram/consumer.clj
@@ -1,6 +1,13 @@
 (ns echo-graalvm.diplomat.telegram.consumer
   (:require [telegrama.api :as api]))
 
+(defn handle-help!
+  [{:keys [event components]}]
+  (let [token (-> components :config :telegram :token)
+        chat-id (-> event :identification :id)
+        message (str "• /help — Show this list of commands. \n• /test — Check if the bot is active and responsive. \n• /chat-id — Reveal your unique Telegram Chat ID.")]
+    (api/send-text! token chat-id message (:http-client components))))
+
 (defn handle-test!
   [{:keys [event components]}]
   (let [token (-> components :config :telegram :token)
@@ -14,5 +21,6 @@
         message (str "Your chat-id is: " chat-id)]
     (api/send-text! token chat-id message (:http-client components))))
 
-(def settings {:bot-command {:test    {:handler handle-test!}
+(def settings {:bot-command {:help    {:handler handle-help!}
+                             :test    {:handler handle-test!}
                              :chat-id {:handler handle-chat-id!}}})
